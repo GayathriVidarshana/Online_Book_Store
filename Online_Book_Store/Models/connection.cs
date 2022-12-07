01697764c7135ad5fa0b3aae30b4b8a9481caa42
IT19094328_Vidarshana_K.L.G.T <IT19094328@my.sliit.lk>
2022-12-07 10:35:15 +0530

    create connection file

--- Online_Book_Store/Models/connection.cs
+++ Online_Book_Store/Models/connection.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +12,40 @@ namespace Online_Book_Store.Models
     public class connection
     {
         private SqlConnection Con;
-        p
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
+        public connection() {
+
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\pc\Documents\BookStore.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConStr);
+            cmd = new SqlCommand();
+            cmd.Connection = Con; 
+
+        }
+
+        public DataTable GetData(string Query) {
+
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConStr);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int SetDate(string Query)
+        {
+            int cnt = 0;
+            if (Con.State == ConnectionState.Closed) {
+
+                Con.Open();
+            }
+            cmd.CommandText = Query;
+            cnt = cmd.ExecuteNonQuery();
+            Con.Close();
+            return cnt;
+        }
 
     }
 }
